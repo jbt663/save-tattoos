4d7b3138065361ff98554559537183d55a7be830
Bill Keller <bill@billkeller.name>
2018-05-10 20:05:53 -0400

    Donate page fleshed out a bit

---
+++ js/donate.js
@@ -0,0 +1,10 @@
+console.log('donate JS file')
+
+// put donate button selector in a var
+var donateButton = document.getElementById('js-submit');
+
+// on click of donate button
+// get value of #js-shop <select>
+// get value of #js-amount <input>
+
+// update DOM with 'Thanks for donating #js-amount.value to #js-shop.value'
